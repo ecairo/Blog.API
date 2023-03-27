4ee8e8ada16a06ed2d3a337c33cb8c276f51b7df
Cairo <3299140+ecairo@users.noreply.github.com>
2023-03-27 22:29:43 +0200

    Convert to normal program setup
    Also add a production settings file

--- Blog.API/Program.cs
+++ Blog.API/Program.cs
@@ -1,27 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace Blog.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+            // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+            var app = builder.Build();
 
-// Necesitamos la definición Open API siempre
-app.UseSwagger();
+            // Necesitamos la definición Open API siempre
+            app.UseSwagger();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{    
-    app.UseSwaggerUI();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
