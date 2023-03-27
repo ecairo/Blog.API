fbc4361f8a36dd5c89e803bf9a79110d770ec639
Cairo <3299140+ecairo@users.noreply.github.com>
2023-03-27 16:03:33 +0200

    Add HeartBeatController
    Basic setup

---
+++ Blog.API/Controllers/HeartBeatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/heartbeat")]
+    [ApiController]
+    public class HeartBeatController : ControllerBase
+    {
+        [HttpGet]
+        public string Beating()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
